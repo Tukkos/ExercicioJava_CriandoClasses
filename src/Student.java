e8dd66b27f8748f81893ff0231ce86653c6b16f7
Tukkos <jpfcastro@id.uff.br>
2023-03-24 09:37:54 -0300

    feat: private setting to all atributes

--- src/Student.java
+++ src/Student.java
@@ -1,16 +1,21 @@
 public class Student {
-    String name;
+    private String name;
     private String cpf;
     private String email;
     Subject subject = new Subject();
 
     public void setCPF(String cpf) {
-        this.cpf = cpf;
+        if (cpf.length() == 11) {
+            this.cpf = cpf;
+        } else {
+            System.out.println("Favor inserir somente os numeros do cpf");
+        }
     }
 
     public String getCPF() {
         return this.cpf;
     }
+
 
     public void setEmail(String email) {
         this.email = email;
@@ -19,4 +24,19 @@ public class Student {
     public String getEmail() {
         return this.email;
     }
+
+
+    public void setName(String name) {
+        if (name.length() > 10) {
+            this.name = name;
+        } else {
+            System.out.println("Favor inserir seu nome completo");
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    
 }
--- src/Subject.java
+++ src/Subject.java
@@ -1,8 +1,8 @@
 public class Subject {
-    String subjectName;
-    String subjectProfessor;
-    int subjectLevel;
-    String[] subjectProgram;
+    private String subjectName;
+    private String subjectProfessor;
+    private int subjectLevel;
+    private String[] subjectProgram;
     private double studentFinalGrade;
     private boolean studentApproval = false;
 
@@ -14,6 +14,7 @@ public class Subject {
         return this.studentFinalGrade;
     }
 
+    
     public void setStudentApproval() {
         if (getStudentFinalGrade() > 70) {
             this.studentApproval = true;
@@ -23,4 +24,50 @@ public class Subject {
     public boolean getStudentApproval() {
         return this.studentApproval;
     }
+
+
+    public void setSubjectName(String subjectName) {
+        if (subjectName.length() > 3) {
+            this.subjectName = subjectName;
+        } else {
+            System.out.println("Favor inserir o nome inteiro da disciplina");
+        }
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+
+    public void setSubjectProfessor(String subjectProfessor) {
+        if (subjectProfessor.length() > 10) {
+            this.subjectProfessor = subjectProfessor;
+        } else {
+            System.out.println("Favor inserir o nome completo do professor");
+        }
+    }
+
+    public String getSubjectProfessor() {
+        return subjectProfessor;
+    }
+
+
+    public void setSubjectLevel(int subjectLevel) {
+        this.subjectLevel = subjectLevel;
+    }
+
+    public int getSubjectLevel() {
+        return subjectLevel;
+    }
+    
+
+    public void setSubjectProgram(String[] subjectProgram) {
+        this.subjectProgram = subjectProgram;
+    }
+
+    public String[] getSubjectProgram() {
+        return subjectProgram;
+    }
+
+    
 }
